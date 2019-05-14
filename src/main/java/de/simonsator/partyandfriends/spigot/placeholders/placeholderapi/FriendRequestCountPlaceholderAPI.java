daf63dfa3a432b81107da786bf8a0ca71dbf0d7c
Simonsator <00pflaume@gmail.com>
2019-05-14 09:35:31 +0200

    Removed use of deprecated api

--- src/main/java/de/simonsator/partyandfriends/spigot/placeholders/placeholderapi/FriendRequestCountPlaceholderAPI.java
+++ src/main/java/de/simonsator/partyandfriends/spigot/placeholders/placeholderapi/FriendRequestCountPlaceholderAPI.java
@@ -1,15 +1,39 @@
 package de.simonsator.partyandfriends.spigot.placeholders.placeholderapi;
 
 import de.simonsator.partyandfriends.spigot.api.FriendRequestCountPlaceHolder;
-import me.clip.placeholderapi.external.EZPlaceholderHook;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-public class FriendRequestCountPlaceholderAPI extends EZPlaceholderHook implements FriendRequestCountPlaceHolder {
+public class FriendRequestCountPlaceholderAPI extends PlaceholderExpansion implements FriendRequestCountPlaceHolder {
 	private final boolean IS_ONLINE_SERVER;
 
+	@Override
+	public boolean persist() {
+		return true;
+	}
+
+	@Override
+	public boolean canRegister() {
+		return true;
+	}
+
+	@Override
+	public String getAuthor() {
+		return "Simonsator";
+	}
+
+	@Override
+	public String getIdentifier() {
+		return "friendsapi";
+	}
+
+	@Override
+	public String getVersion() {
+		return "1.0";
+	}
+
 	public FriendRequestCountPlaceholderAPI(Plugin pPlugin) {
-		super(pPlugin, "friendsapi");
 		IS_ONLINE_SERVER = pPlugin.getConfig().getBoolean("IsOnlineServer");
 	}
 
